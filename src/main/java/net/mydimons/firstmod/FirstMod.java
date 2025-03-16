51292a28b438ebd0c57b2200166c1ce1bdd8f40c
mydimons <mydimons.kb@gmail.com>
2025-03-15 17:45:02 -0700

    Initial commit

---
+++ src/main/java/net/mydimons/firstmod/FirstMod.java
@@ -0,0 +1,15 @@
+package net.mydimons.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
